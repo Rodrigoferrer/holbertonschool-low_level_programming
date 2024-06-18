bb2404c5717b35d76222ced2e5225d55cba5467a
RodrigoFerrer <rodrigoferrer30@gmail.com>
2024-06-18 20:37:15 +0000

    3-islower.c

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,12 +1,14 @@
 #include "main.h"
+/**
+ * _islower - Entry point
+ * @c: character
+ * Return: 1 (Success) - 0 (Otherwise)
+ */
 
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-
 		return (1);
 	else
 		return (0);
-	
 }
-
