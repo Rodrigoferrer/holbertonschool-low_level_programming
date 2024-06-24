25f1af79883037dd81bc3f175aeb1e40eee9b500
Rodrigo Ferrer <rodrigoferrer30@gmail.com>
2024-06-24 17:15:50 +0000

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * function that prints a string backwards
+ * return: always 0
+ */
+void print_rev(char *s)
+{
+    int longitud = strlen(s);
+    int i;
+    for (i = longitud - 1; i >= 0; i--) {
+        putchar(s[i]);
+    }
+    putchar('\n');
+}
