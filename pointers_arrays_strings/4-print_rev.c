d6c62260b70fa3bc788b192e565ea0856c6096bd
Rodrigo Ferrer <rodrigoferrer30@gmail.com>
2024-06-24 17:17:14 +0000

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -11,7 +11,7 @@ void print_rev(char *s)
     int longitud = strlen(s);
     int i;
     for (i = longitud - 1; i >= 0; i--) {
-        putchar(s[i]);
+        _putchar(s[i]);
     }
-    putchar('\n');
+    _putchar('\n');
 }
