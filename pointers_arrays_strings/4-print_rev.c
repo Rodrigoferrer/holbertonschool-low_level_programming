fb9c6850737ecdfb01470b50ce5ab0d654874465
Rodrigo Ferrer <rodrigoferrer30@gmail.com>
2024-06-24 18:43:39 +0000

    4-print_rev

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,15 +1,17 @@
-#include "string.h"
 #include "main.h"
+#include "2-strlen.c"
 /**
  * function that prints a string backwards
  * return: always 0
  */
 void print_rev(char *s)
 {
-    int longitud = strlen(s);
-    int i;
-    for (i = longitud - 1; i >= 0; i--) {
-        _putchar(s[i]);
-    }
-    _putchar('\n');
+	int len = _strlen(s);
+
+	while (len >= 0) 
+	{
+		_putchar(s[len]);
+		len--;
+	}
+		_putchar('\n');
 }
