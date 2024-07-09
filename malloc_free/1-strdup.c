13d4270d711335e8af7c7e9101f20152210b6b8c
Rodrigo <rodrigoferrer30@gmail.com>
2024-07-09 14:39:38 +0000

    1-strdup.c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+char *_strdup(char *str)
+{
+	char *duplicado;
+	int longitud_str;
+	int i;
+
+	longitud_str = _strlen(str);
+	duplicado = malloc(longitud_str + 1 * sizeof(char));
+	if (str == NULL)
+	{
+	return (NULL);
+	}
+	for (i = 0; i < longitud_str; i++)
+	{
+	duplicado[i] = str[i];
+	}
+	duplicado[longitud_str] = '\0';
+	
+	return(duplicado);
+}
---
+++ malloc_free/_strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * funcion que devuelve el largo de un string
+ */
+int _strlen(char *s)
+{
+	int contador = 0;
+
+	for (; s[contador] != '\0'; contador++)
+	{
+	}
+
+	return (contador);
+}
