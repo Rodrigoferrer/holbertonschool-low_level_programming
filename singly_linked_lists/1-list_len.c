aebd709689958a42e96ade21b149a2d37f053b39
root <rodrigoferrer30@gmail.com>
2024-08-02 14:16:10 +0000

    1-list_len.c

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * 
+ * Imprimir todos los elementos de la lista
+ * Si str = NULL , Print [0] (Nil)
+ * @h: Head of linked list
+ * Return: Number of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	int contador = 0;
+
+	while (h != NULL)
+	{
+		contador++;
+		h = h->next;
+	}
+	return (contador);
+}
