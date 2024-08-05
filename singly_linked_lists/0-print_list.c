3970f3b7442068d4daa8427e61ca277bd6d82bd9
Rodrigo <rodrigoferrer30@gmail.com>
2024-08-05 12:36:54 +0000

    0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -17,20 +17,19 @@
  * @return Número de nodos en la lista.
  */
 
-
 size_t print_list(const list_t *h)
 {
-	size_t contador = 0;
+    size_t count = 0;
 
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-		printf("[0] (nil)\n");
-	else
-		printf("[%u] %s\n", h->len, h->str);
-	contador++;
-	h = h->next;
-	}
+    while (h != NULL)
+    {
+        if (h->str == NULL)
+            printf("[0] (nil)\n");
+        else
+            printf("[%lu] %s\n", h->len, h->str);
+        count++;
+        h = h->next;
+    }
 
-return (contador);
+    return count;
 }
