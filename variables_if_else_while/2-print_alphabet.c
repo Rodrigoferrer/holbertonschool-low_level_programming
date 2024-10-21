784ecb146ac5be9fced97f5aab7229096cc0775f
Rodrigo <rodrigoferrer30@gmail.com>
2024-10-21 16:19:35 +0000

    2-print_alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Program that gives the alphabet from a to z
+ * Return: 0 if succesfull
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i < 'z'; i++)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
