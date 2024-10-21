2245af5f790d17d7b170b10da758a9d43d026e2e
Rodrigo <rodrigoferrer30@gmail.com>
2024-10-21 17:55:02 +0000

    3-print_alphabets

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -8,11 +8,15 @@
 int main(void)
 {
 	char i;
+	char j;
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
-	for (i = 'A'; i <= 'Z'; i++);
 	putchar(i);
+	}
+	for (j = 'A'; j <= 'Z'; j++)
+	{
+	putchar(j);
 	}
 	putchar('\n');
 	return (0);
