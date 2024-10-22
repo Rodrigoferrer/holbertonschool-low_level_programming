cefe7b7aefefcfd91af81bcf2185b5cd42b6bf79
Rodrigo <rodrigoferrer30@gmail.com>
2024-10-22 14:46:10 +0000

    9-print_comb

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -12,13 +12,13 @@ int main(void)
 	while (num <= 9)
 	{
 		putchar(num + '0');
-		if(num != 9)
+		if (num != 9)
 		{
-			putchar(' ');
 			putchar(',');
+			putchar(' ');
 		}
 		num++;
-	}	
+	}
 	putchar ('\n');
 	return (0);
 }
