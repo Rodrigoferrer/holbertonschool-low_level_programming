115ee8143b24c691d6d484b108681e3cc335a4dc
Rodrigo <rodrigoferrer30@gmail.com>
2024-10-26 22:20:44 +0000

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -11,22 +11,27 @@
 void times_table(void)
 {
 	int i, j, product;
-	
+
 	for (i = 0; i < 10; i++)
 	{
-	for (j = 0; j < 10; j++)
-	{	
-	product = i * j;
-	_putchar (product + '0');
-	if (product != 0)
-	{
-		_putchar(' ');
+		for (j = 0; j < 10; j++)
+		{
+		product = i * j;
+		if (j != 0)
+		{
 		_putchar(',');
-	}
-	if (product > 10)
-	{	
-	_putchar(product + '0');
-	}
-	}
+		_putchar(' ');
+		}
+		if (product < 10)
+		{
+			_putchar(' ');
+		}
+		if (product >= 10)
+		{
+			_putchar((product / 10) + '0');
+			}
+			_putchar((product % 10) + '0');
+		}
+		_putchar('\n');
 	}
 }
