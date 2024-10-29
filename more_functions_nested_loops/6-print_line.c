4be158943003597b3cb6bfc991cf8e31740570a5
Rodrigo <rodrigoferrer30@gmail.com>
2024-10-29 16:02:25 +0000

    6-print_line.c

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -9,18 +9,16 @@
 
 void print_line(int n)
 {
-    int i;
+	int i;
 
-    if (n <= 0) 
-    {
-        _putchar('\n'); 
-        return; 
-    }
-
-    for (i = 0; i < n; i++) 
-    {
-        _putchar('_'); 
-    }
-
-    _putchar('\n'); 
+	if (n <= 0)
+	{
+	_putchar('\n');
+	return;
+	}
+	for (i = 0; i < n; i++)
+	{
+	_putchar('_');
+	}
+	_putchar('\n');
 }
