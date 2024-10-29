77772655d165411a1b4f2ff46656e94fcaa3653f
Rodrigo <rodrigoferrer30@gmail.com>
2024-10-29 17:02:25 +0000

    7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -12,15 +12,17 @@ void print_diagonal(int n)
 	int i;
 	int j;
 	
-	if (n > 0);
-	
-	for (i = 1; i <= n; i++)
+	if (n > 0)
 	{
-		_putchar('\\');
-		_putchar('\n');
-		for (j = 1; j <= i; j++)
+	
+		for (i = 1; i <= n; i++)
 		{
-			_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+			for (j = 1; j <= i; j++)
+			{
+				_putchar(' ');
+			}
 		}
 	}
 	else
