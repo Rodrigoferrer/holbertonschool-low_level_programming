948b19e436bf2c9b71b7104cb165ae4ba62deddf
Rodrigo <rodrigoferrer30@gmail.com>
2024-10-30 14:18:33 +0000

    9-fizz_buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -2,38 +2,24 @@
 #include <stdio.h>
 
 /**
- * fizz_buzz - Function that prints number from 0 to 10
- * @n: Parameter to be checked
- * Return: Void
- */
-
-void fizz_buzz(int n)
-{
-	int i;
-
-	for (i = n; i <= 100; i++)
-	{
-		if (n % 3 == 0)
-		printf("Fizz");
-		if (n % 5 == 0)
-		printf("Buzz");
-		if (n % 3 == 0 && n % 5 == 0)
-			printf("FizzBuzz");
-		if (n < 100)
-		printf(" ");
-	}
-}
-/**
  * main - Entry of the program
  * Return: Always 0
  */
 
 int main(void)
 {
-	int n;
+	int i;
 
-	n = 1;
-
-	fizz_buzz(n);
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		printf("Fizz");
+		if (i % 5 == 0)
+		printf("Buzz");
+		if (i % 3 == 0 && i % 5 == 0)
+		printf("FizzBuzz");
+		if (i < 100)
+		printf(" ");
+	}
 	return (0);
 }
