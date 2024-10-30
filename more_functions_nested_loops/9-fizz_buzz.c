0686333b26ccb4ab81d3485885f917f562ead6db
Rodrigo <rodrigoferrer30@gmail.com>
2024-10-30 14:23:19 +0000

    9-fizz_buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -6,20 +6,29 @@
  * Return: Always 0
  */
 
-int main(void)
+void fizz_buzz(int n)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = n; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (n % 3 == 0)
 		printf("Fizz");
-		if (i % 5 == 0)
+		if (n % 5 == 0)
 		printf("Buzz");
-		if (i % 3 == 0 && i % 5 == 0)
+		if (n % 3 == 0 && i % 5 == 0)
 		printf("FizzBuzz");
-		if (i < 100)
+		if (n < 100)
 		printf(" ");
 	}
+}
+
+int main(void)
+{
+	int h;
+
+	h = 1;
+
+	fizz_buzz(h);
 	return (0);
 }
