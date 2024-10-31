fe24067da5d2d47c0979545d5f50628d388bd978
Rodrigo <rodrigoferrer30@gmail.com>
2024-10-31 17:17:50 +0000

    8-print_array.c

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,4 @@
-#include "main.h" 
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -8,15 +8,18 @@
  * Return: Void
  */
 
+
 void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; a[i] < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
 		if (i < n - 1)
-			printf(" ,");
+		{
+		printf(" ,");
+		}
 	}
 	printf("\n");
 }
