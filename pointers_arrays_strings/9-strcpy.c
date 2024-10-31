bfb97eb30b561bd413ad7a4aadc9934454c6e5e7
Rodrigo <rodrigoferrer30@gmail.com>
2024-10-31 17:57:54 +0000

    9-strcpy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -12,14 +12,16 @@ char *_strcpy(char *dest, char *src)
 {
 	int len, i;
 
-	len = 0
-	
+	len = 0;
+
 	while (src[len] != '\0')
 	{
 		len++;
 	}
 	for (i = 0; i < len; i++)
 	{
-	src[i] = dest[i]
+	dest[i] = src[i];
 	}
+	dest[i] = '\0';
+	return (dest);
 }
