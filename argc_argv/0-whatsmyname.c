7aaaecc0323fa3238225cc82a26a17e8111d3af3
Rodrigo <rodrigoferrer30@gmail.com>
2024-11-07 15:50:43 +0000

    0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _argc_argv - Function that prints the name of the program
+ * @argc: Arguments count
+ * @argv: Arguments variables
+ * Return: Void
+ */
+
+void _argc_argv(int argc, char **argv)
+{
+	printf("The name of the program is: %s\n", argv[0]);
+}
+
+/**
+ * main - Entry of the program
+ * @argc: arguments count
+ * @argv: arguments variables
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	_argc_argv(argc, argv);
+		return (0);
+}
---
+++ argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void _argc_argv(int argc, char **argv);
+
+#endif
