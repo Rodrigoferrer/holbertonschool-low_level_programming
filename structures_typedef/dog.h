b2494c1ed79e602dbef1e6680a3c38d91e5811f9
Rodrigo <rodrigoferrer30@gmail.com>
2024-11-15 16:44:26 +0000

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct_dog - Struct that saves the name age and owner of dog
+ * @name: name of the owner
+ * @age: Age of owner
+ * @owner: Owner
+ * Return: Nothing
+ */
+
+struct dog my_dog
+{
+	char *name, owner;
+	float age;
+}
