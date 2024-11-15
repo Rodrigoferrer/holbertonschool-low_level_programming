cc6c5c45972164a9a673c800c92c447fd8e78321
Rodrigo <rodrigoferrer30@gmail.com>
2024-11-15 18:25:39 +0000

    3-main.c

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -19,4 +19,17 @@ struct dog
 ;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+ * struct dog_t - Struct that saves the name age and owner of dog
+ * @name: name of the owner
+ * @age: Age of owner
+ * @owner: Owner
+ * Description: Struct
+ */
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
 #endif
