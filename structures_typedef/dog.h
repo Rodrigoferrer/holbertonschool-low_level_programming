91bfae5e72d4fe47c1fbaf8e9480a6b24742e2cb
Rodrigo <rodrigoferrer30@gmail.com>
2024-11-15 16:53:22 +0000

    dog.h.2

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct_dog - Struct that saves the name age and owner of dog
+ * struct dog - Struct that saves the name age and owner of dog
  * @name: name of the owner
  * @age: Age of owner
  * @owner: Owner
