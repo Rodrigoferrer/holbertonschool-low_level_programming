be22f9a09866e10b1f75e72c4d8276fc2290ebf4
Rodrigo <rodrigoferrer30@gmail.com>
2024-11-18 17:05:04 +0000

    3-get_op_func.c.2nd.try

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -21,15 +21,10 @@ int (*get_op_func(char *s))(int, int)
 
 	i = 0;
 
-	if (s == NULL || s[0] == '\0')
-		return (NULL);
-
-	while (ops[i].op != NULL)
+	while (s && s[0] != '\0' && ops[i].op)
 	{
 		if (s[0] == ops[i].op[0])
-		{
 			return (ops[i].f);
-		}
 		i++;
 	}
 	return (NULL);
