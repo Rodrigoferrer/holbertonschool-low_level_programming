7914d24744501b5d92ec78fd1d5daff2c2e743db
Rodrigo <rodrigoferrer30@gmail.com>
2024-11-25 00:26:41 +0000

    1-print_numbers.c.corregido

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -11,30 +11,20 @@
  * @n: number of integers passed to the function
  * Return: Void
  */
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
-	unsigned int current_value;
 
 	va_start(args, n);
-	i = 0;
 
-	if (separator == NULL || n == 0)
-	{	va_end(args);
-		return;
-	}
-	while (i < n)
+	for (i = 0; i < n; i++)
 	{
-		current_value = va_arg(args, int);
-		printf("%d", current_value);
-		if (i < n - 1 && separator != NULL)
-		{
+		printf("%d", va_arg(args, int));
+		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
-		}
-		i++;
 	}
+
 	va_end(args);
 	printf("\n");
 }
