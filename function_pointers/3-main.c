8e3eb76f9050852e76aedb6e37b65a335772e072
Rodrigo <rodrigoferrer30@gmail.com>
2024-11-25 00:41:00 +0000

    3-betty.corregido

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -10,31 +10,31 @@
  */
 int main(int argc, char *argv[])
 {
-        int num1, num2;
-        int (*operation)(int, int);
+	int num1, num2;
+	int (*operation)(int, int);
 
-        if (argc != 4)
-        {
-                printf("Error\n");
-                exit(98);
-        }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-        num1 = atoi(argv[1]);
-        num2 = atoi(argv[3]);
-        operation = get_op_func(argv[2]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operation = get_op_func(argv[2]);
 
-        if (!operation || argv[2][1] != '\0')
-        {
-                printf("Error\n");
-                exit(99);
-        }
+	if (!operation || argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-        if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
-        {
-                printf("Error\n");
-                exit(100);
-        }
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-        printf("%d\n", operation(num1, num2));
-        return (0);
+	printf("%d\n", operation(num1, num2));
+	return (0);
 }
