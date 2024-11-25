fc4d7defcf02158304bf931d583207bb3d9ed954
Rodrigo <rodrigoferrer30@gmail.com>
2024-11-25 00:33:15 +0000

    2-int_index.c.corregido

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -15,17 +15,23 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	i = 0;
 
+	if (array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
+
 	if (size <= 0)
 	{
 		return (-1);
 	}
+
 	while (i < size)
 	{
 		if (cmp(array[i]) != 0)
 		{
 			return (i);
 		}
-			i++;
+		i++;
 	}
 	return (-1);
 }
